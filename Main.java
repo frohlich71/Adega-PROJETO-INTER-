69daedd9a1958a75dcf21ae582d608e23fcaf14a
Victor Frohlich <victorfrohlich520@gmail.com>
2022-11-02 15:40:54 -0300

    teste

--- Main.java
+++ Main.java
@@ -21,8 +21,6 @@ class Main {
     clientes.add(joao);
     funcionarios.add(Cesar);
 
-    Cliente cliente = new Cliente();
-
     while (menu != 10) {
       System.out.println("\nMenu Principal\n");
       System.out.println(" 1) Listar clientes\n");
@@ -39,11 +37,8 @@ class Main {
       menu = input.nextInt();
 
       switch (menu) {
-        case 1:
-          clientes.forEach((value) -> System.out.println(value.toString()));
-          break;
-
-        case 2:
+        case 1 -> clientes.forEach((value) -> System.out.println(value.toString()));
+        case 2 -> {
           System.out.println("Digite seu nome: ");
           String nome = input.next();
           System.out.println("Digite seu cpf: ");
@@ -52,13 +47,10 @@ class Main {
           String telefone = input.next();
           System.out.println("Digite seu endereço: ");
           String endereco = input.next();
-
           Cliente NovoCliente = new Cliente(nome, cpf, telefone, endereco);
           clientes.add(NovoCliente);
-
-          break;
-
-        case 3:
+        }
+        case 3 -> {
           System.out.println("Digite o nome do produto: ");
           String produtoNome = input.next();
           System.out.println("Digite o teor alcolico do produto: ");
@@ -67,51 +59,29 @@ class Main {
           String validade = input.next();
           System.out.println("Digite o preço do produto: ");
           double preco = input.nextDouble();
-
           Produto NovoProduto = new Produto(produtoNome, teorAlcoolico, validade, preco);
           produtos.add(NovoProduto);
-
-          break;
-
-        case 6:
-
+        }
+        case 6 -> {
           clientes.forEach((value) -> System.out.println(value.toString()));
-
           System.out.println("Digite qual cliente deseja remover: ");
           int cliente_para_remover = input.nextInt();
-
           clientes.remove(cliente_para_remover - 1);
-
-          break;
-
-        case 7:
-
+        }
+        case 7 -> {
           produtos.forEach((value) -> System.out.println(value.toString()));
-
           System.out.println("Digite qual produto deseja remover: ");
           int produto_para_remover = input.nextInt();
-
           produtos.remove(produto_para_remover - 1);
-
-          break;
-
-        case 8:
-
-          funcionarios.forEach((value) -> value.imprimeDados());
-
+        }
+        case 8 -> {
+          funcionarios.forEach(Funcionario::imprimeDados);
           System.out.println("Digite qual funcionario deseja remover: ");
           int funcionario_para_remover = input.nextInt();
-
           funcionarios.remove(funcionario_para_remover - 1);
-
-          break;
-
-        case 9:
-          produtos.forEach((value) -> System.out.println(value.toString()));
-
-          break;
-
-        case 4:
+        }
+        case 9 -> produtos.forEach((value) -> System.out.println(value.toString()));
+        case 4 -> {
           System.out.println("Digite o nome do funcionario: ");
           String NovoNome = input.next();
           System.out.println("Digite a mátricula do funcionario: ");
@@ -120,30 +90,19 @@ class Main {
           double NovoSalario = input.nextDouble();
           System.out.println("Cargo: \n 1) Gerente \n 2) Vendedor");
           int cargo = input.nextInt();
+          if (cargo == 1) {
 
-          if(cargo == 1) {
-            
             Gerente NovoGerente = new Gerente(NovoNome, NovaMatricula, NovoSalario);
             funcionarios.add(NovoGerente);
-          } else if(cargo == 2) {
-            
+          } else if (cargo == 2) {
+
             Vendedor NovoVendedor = new Vendedor(NovoNome, NovaMatricula, NovoSalario, 0);
             funcionarios.add(NovoVendedor);
           }
-          break;
-
-        case 5:
-          funcionarios.forEach((value) -> value.imprimeDados());
-          break;
-
-        case 10:
-          System.out.print("Programa encerrado");
-          menu = 10;
-          break;
-
-        default:
-          System.out.println("default ");
-          break;
+        }
+        case 5 -> funcionarios.forEach(Funcionario::imprimeDados);
+        case 10 -> System.out.print("Programa encerrado");
+        default -> System.out.println("default ");
       }
     }
     input.close();
